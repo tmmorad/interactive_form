40ce70f422bcbb6ad26ef326dc48ee020804e2b7
Tamer <32287010+tmmorad@users.noreply.github.com>
2018-11-12 21:36:16 -0800

    intial commit update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 3 - Interactive Form
+Tamer Morad
+******************************************/
+
+function setFocus(){
+  document.querySelector("user_name").focus();
+}
